57c1672b8924c4c368cf45f5d786d18d92bd07c1
Kent Wood <minzojian@hotmail.com>
2022-12-19 16:51:23 +0800

    做了一些修改,支持了连接箭头重新拖动改变连接对象

--- src/App.vue
+++ src/App.vue
@@ -28,71 +28,71 @@ import MyNode from "@/components/my-node.vue";
 import { Graph, Markup, Path } from "@antv/x6";
 import { register } from "@antv/x6-vue-shape";
 import { Dom } from "@antv/x6-common";
+// import { Core } from "@antv/x6-common/es/dom/event/core";
 import { ContextMenuTool } from "./components/ContextMenuTool.ts";
 
-const degreesToRadians = (degrees) => degrees * (Math.PI / 180);
-const transCanvasAngleToRos = (angle) => angle - 90;
+// const degreesToRadians = (degrees) => degrees * (Math.PI / 180);
+// const transCanvasAngleToRos = (angle) => angle - 90;
 
-const calcPoint = (p) => {
-  const distance = 10;
-  const theta = degreesToRadians(transCanvasAngleToRos(p.angle));
-  const x = p.x + distance * Math.cos(theta);
-  const y = p.y + distance * Math.sin(theta);
-  return { x, y };
-};
+// const calcPoint = (p) => {
+//   const distance = 10;
+//   const theta = degreesToRadians(transCanvasAngleToRos(p.angle));
+//   const x = p.x + distance * Math.cos(theta);
+//   const y = p.y + distance * Math.sin(theta);
+//   return { x, y };
+// };
 
-const calcIntersectionPoint = (points) => {
-  const [p1, p2] = points;
-  const p11 = calcPoint(p1);
-  const p22 = calcPoint(p2);
-  const d1 = (p1.x - p11.x) * (p2.y - p22.y);
-  const d2 = (p1.y - p11.y) * (p2.x - p22.x);
-  const d = d1 - d2;
+// const calcIntersectionPoint = (points) => {
+//   const [p1, p2] = points;
+//   const p11 = calcPoint(p1);
+//   const p22 = calcPoint(p2);
+//   const d1 = (p1.x - p11.x) * (p2.y - p22.y);
+//   const d2 = (p1.y - p11.y) * (p2.x - p22.x);
+//   const d = d1 - d2;
 
-  if (d === 0) {
-    return { x: p1.x, y: p1.y };
-  }
+//   if (d === 0) {
+//     return { x: p1.x, y: p1.y };
+//   }
 
-  const u1 = p1.x * p11.y - p1.y * p11.x;
-  const u4 = p2.x * p22.y - p2.y * p22.x;
+//   const u1 = p1.x * p11.y - p1.y * p11.x;
+//   const u4 = p2.x * p22.y - p2.y * p22.x;
 
-  const u2x = p2.x - p22.x;
-  const u3x = p1.x - p11.x;
-  const u2y = p2.y - p22.y;
-  const u3y = p1.y - p11.y;
+//   const u2x = p2.x - p22.x;
+//   const u3x = p1.x - p11.x;
+//   const u2y = p2.y - p22.y;
+//   const u3y = p1.y - p11.y;
 
-  const px = (u1 * u2x - u3x * u4) / d;
-  const py = (u1 * u2y - u3y * u4) / d;
+//   const px = (u1 * u2x - u3x * u4) / d;
+//   const py = (u1 * u2y - u3y * u4) / d;
 
-  const p = { x: px, y: py };
+//   const p = { x: px, y: py };
 
-  return p;
-};
+//   return p;
+// };
 
-Graph.registerConnector(
-  "curve",
-  (sourcePoint, targetPoint) => {
-    const path = new Path();
-    path.appendSegment(Path.createSegment("M", sourcePoint));
-    // 基于方向获取控制点
-    const controlPoint = calcIntersectionPoint([
-      {
-        ...sourcePoint,
-        angle: graph.getNodesFromPoint(sourcePoint)[0]?.getAngle() || 0,
-      },
-      {
-        ...targetPoint,
-        angle: graph.getNodesFromPoint(targetPoint)[0]?.getAngle() || 0,
-      },
-    ]);
-    // 基于控制点生成二次贝塞尔曲线
-    path.appendSegment(path.quadTo(controlPoint, targetPoint));
-    return path.serialize();
-  },
-  true
-);
+// Graph.registerConnector(
+//   "curve",
+//   (sourcePoint, targetPoint, routePoints, options, view) => {
+//     const path = new Path();
+//     path.appendSegment(Path.createSegment("M", sourcePoint));
+//     // 基于方向获取控制点
+//     const controlPoint = calcIntersectionPoint([
+//       {
+//         ...sourcePoint,
+//         angle: view.graph.getNodesFromPoint(sourcePoint)[0]?.getAngle() || 0,
+//       },
+//       {
+//         ...targetPoint,
+//         angle: view.graph.getNodesFromPoint(targetPoint)[0]?.getAngle() || 0,
+//       },
+//     ]);
+//     // 基于控制点生成二次贝塞尔曲线
+//     path.appendSegment(path.quadTo(controlPoint, targetPoint));
+//     return path.serialize();
+//   },
+//   true
+// );
 
-window["Dom"] = Dom;
 register({
   shape: "my-node",
   width: 100,
@@ -239,57 +239,11 @@ export default {
               },
             },
           })),
-          // [
-          // {
-          //   id: "port11",
-          //   group: "group1",
-          //   args: {
-          //     width: Dom.measureText("肯定").width+8,
-          //   },
-          //   attrs: {
-          //     fo:{width: Dom.measureText("肯定").width+8},
-          //     label: {
-          //       text: "肯定",
-          //       style: { background: "blue", color: "white" },
-          //     },
-          //   },
-          // },
-          // {
-          //   id: "port22",
-          //   group: "group1",
-          //   args: {
-          //     width: Dom.measureText("肯定2").width+8,
-          //   },
-          //   attrs: {
-          //     fo:{width: Dom.measureText("肯定2").width+8},
-          //     label: {
-          //       text: "肯定2",
-          //       style: { background: "red", color: "white" },
-          //     },
-          //   },
-          // },
-          // {
-          //   id: "port33",
-          //   group: "group1",
-          //   args: {
-          //     width: Dom.measureText("肯定22").width+8,
-          //   },
-          //   attrs: {
-          //     fo:{width: Dom.measureText("肯定22").width+8},
-          //     label: {
-          //       text: "肯定22",
-          //       style: { background: "red", color: "white" },
-          //     },
-          //   },
-          // },
-          // ],
         },
       });
     },
 
     addPortInNode(nodeId, portLabel) {
-      // const type=portColors[portLabel]||portColors['默认'];
-
       this.graph.getCellById(nodeId).addPort({
         group: "group1",
 
@@ -320,19 +274,15 @@ export default {
         },
         background: { color: "#ddd" },
         grid: true,
+        interacting: {
+          edgeMovable: true,
+          arrowheadMovable: true,
+        },
         connecting: {
           allowLoop: false,
-          // highlight:true,
           targetAnchor: "top",
           sourceAnchor: "bottom",
-          // router:{
-          // name: "er",
-          // args:{
-          //   startDirections:["bottom"],
-          //   endDirections:["top"]
-          // }
-          // },
-          connector: { name: "smooth" },
+          connector: { name: "curve" },
           validateConnection({
             edge,
             edgeView,
@@ -346,7 +296,6 @@ export default {
             targetCell,
             type,
           }) {
-            // console.log(targetPort,targetMagnet,targetCell,type)
             if (targetPort != null && sourcePort != null) {
               //不能用连接桩连接直接另一个连接桩
               return false;
@@ -355,18 +304,12 @@ export default {
           },
         },
         onPortRendered(args) {
-          // args.container.innerHTML='';
           const selectors = args.contentSelectors;
           const container = selectors && selectors.foContent;
           if (container) {
             const c = document.createElement("div");
-            c.id = "ttt";
 
             container.appendChild(c);
-
-            // const r= createApp({render(){return h('div',"hello")}})
-            //   console.log(r);
-            //   r.mount("#ttt")
 
             requestAnimationFrame(() => {
               const type =
@@ -389,40 +332,67 @@ export default {
 
               createApp(RootComponent).mount(c);
             });
-
-            // container.innerHTML =' <el-button type="primary">Primary</el-button>'
-
-            //  `<span
-            //             style="
-            //               padding: 1px 2px;
-            //               border-radius: 4px;
-            //               white-space: nowrap;
-            //               color:${args.port.attrs.label?.style.color||'black'};
-            //               background-color:${args.port.attrs.label?.style.background||'white'};
-            //               "
-            //           >
-            //             ${args.port.attrs.label?.text}
-            //           </span>`;
-            // const holder = container.firstChild;
-
-            // requestAnimationFrame(()=>{
-            // args.node.setPortProp(args.port.id,'attrs/fo/width',holder.offsetWidth)
-            // })
           }
         },
       }));
       window["graph"] = graph;
       graph.on("edge:mouseenter", function (props) {
-        console.log(arguments);
         const { edge, e, cell, view } = props;
         cell.setAttrByPath("line/strokeWidth", 4);
+
+        const portId = view.sourceMagnet.getAttribute("port");
+        const port = view.sourceView.cell.getPort(portId);
+
+        cell.addTools([
+          {
+            name: "target-arrowhead",
+            args: {
+              // fallbackAction: "remove",
+              attrs: {
+                fill: (chalk[
+                  portColors[port.attrs.label?.text] || portColors["默认"]
+                ] || [""])[0],
+              },
+            },
+          },
+        ]);
       });
 
+      // graph.on("edge:mousedown", function (props) {
+      //   console.log(arguments);
+      //   const { edge, e, cell, view } = props;
+      //   if (cell) cell.onMagnetMouseDown(e, view);
+      //   // cell.setAttrByPath("line/strokeWidth", 4);
+      // });
+
       graph.on("edge:mouseleave", function (props) {
-        console.log(arguments);
         const { edge, e, cell, view } = props;
         cell.setAttrByPath("line/strokeWidth", 2);
+        cell.removeTools();
       });
+
+      // graph.on("edge:move", function (props) {
+      //   const { edge, e, cell, view } = props;
+      //   Core.dispatch(view.graph.container, {
+      //     ...e,
+      //     type: "mousedown",
+      //     currentTarget: view.sourceMagnet,
+      //     target: view.sourceMagnet.firstChild,
+      //   });
+      //   // view.sourceMagnet.dispatchEvent(new MouseEvent("mousedown", { ...e }));
+      //   // view.sourceView.setEventData(e, {
+      //   //   action: "magnet",
+      //   //   currentView: view.sourceView,
+      //   // });
+      //   // // e.data.currentView = view.sourceView;
+      //   // e.currentTarget = view.sourceMagnet;
+      //   // view.sourceView.onMagnetMouseDown(
+      //   //   e,
+      //   //   view.sourceView,
+      //   //   e.offsetX,
+      //   //   e.offsetY
+      //   // );
+      // });
 
       graph.on("edge:mousedown", function (props) {
         const { edge, e, cell, view } = props;
@@ -436,80 +406,9 @@ export default {
             (chalk[
               portColors[port.attrs.label?.text] || portColors["默认"]
             ] || [""])[0]
-          ); //
-          console.log(arguments);
+          );
         }, 100);
       });
-      //     graph.addNode({
-      //       shape: "my-node",
-      //       x: 100,
-      //       y: 100,
-      //       width: 200,
-      //       height: 100,
-      //       data: {
-      //         label: "world aklsdjalksdjflkajsdf\nasdfklajdlfjaklsdf",
-      //         style: { color: "blue" },
-      //       },
-      //       tools: [
-      //   {
-      //     name: 'contextmenu',
-      //     args: {
-      //       menu:[
-      //         h(ElMenuItem,{index:1,onClick:()=>{console.log(111)}},'item 1'),
-      //         h(ElMenuItem,{index:2,onClick:()=>{console.log(222)}},'item 2'),
-      //         h(ElMenuItem,{index:3,onClick:()=>{console.log(333)}},'item 3')
-      //       ],
-      //     },
-      //   },
-      // ],
-      //       ports: {
-      //         items: [
-      //           // {
-      //           //   id: "port11",
-      //           //   group: "group1",
-      //           //   args: {
-      //           //     width: Dom.measureText("肯定").width+8,
-      //           //   },
-      //           //   attrs: {
-      //           //     fo:{width: Dom.measureText("肯定").width+8},
-      //           //     label: {
-      //           //       text: "肯定",
-      //           //       style: { background: "blue", color: "white" },
-      //           //     },
-      //           //   },
-      //           // },
-      //           // {
-      //           //   id: "port22",
-      //           //   group: "group1",
-      //           //   args: {
-      //           //     width: Dom.measureText("肯定2").width+8,
-      //           //   },
-      //           //   attrs: {
-      //           //     fo:{width: Dom.measureText("肯定2").width+8},
-      //           //     label: {
-      //           //       text: "肯定2",
-      //           //       style: { background: "red", color: "white" },
-      //           //     },
-      //           //   },
-      //           // },
-      //           // {
-      //           //   id: "port33",
-      //           //   group: "group1",
-      //           //   args: {
-      //           //     width: Dom.measureText("肯定22").width+8,
-      //           //   },
-      //           //   attrs: {
-      //           //     fo:{width: Dom.measureText("肯定22").width+8},
-      //           //     label: {
-      //           //       text: "肯定22",
-      //           //       style: { background: "red", color: "white" },
-      //           //     },
-      //           //   },
-      //           // },
-      //         ],
-      //       },
-      //     });
-      //   },
     },
   },
 };
